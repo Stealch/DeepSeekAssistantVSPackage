289f122bac75927a5f36655b25db855e459ef43e
Vladimir <vokzalka@ya.ru>
2026-01-23 21:27:31 +0300

    Next

---
+++ Providers/DeepSeekChatWindowProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace DeepSeekAssistantVSPackage.Providers
+{
+    [Guid("8a4b1d3c-7e9f-4a5c-8b5a-6c5b550d2a4b")]
+    public class DeepSeekChatWindowProvider
+    {
+        [Microsoft.VisualStudio.Shell.ProvideToolWindow(typeof(ToolWindows.DeepSeekChatWindow))]
+        public void RegisterToolWindow()
+        {
+            // Регистрация через атрибут
+        }
+    }
+}
